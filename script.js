fedef72b635e57bc5c54285a57165adfc947fd4b
OtavioFetter <138801781+OtavioFetter@users.noreply.github.com>
2023-10-04 21:43:44 -0300

    script.js

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-//faz com que a caixa de texto funcione clicando a tecla enter
+//faz com que quando o usuário utilize a tecla enter o valor apareça
 document.getElementById('cep').addEventListener('keypress', function (event) {
     if (event.key === "Enter") {
-        buscarCEP();
+        buscarCEP(); // Chama a função buscarCEP() quando a tecla Enter é pressionada
     }
 });
 
-//faz com que o site retorne um alerta que o mínimo de caracteres é 8
+//serve para buscar o cep
 function buscarCEP() {
-    let cep = document.getElementById('cep').value;
+    let cep = document.getElementById('cep').value; //obtem o cep
+    let mensagem = document.getElementById('mensagem'); //faz exibir mensagens
+    let resultado = document.getElementById('resultado'); //exibi informações do cep
+
+    //verifica se o cep possui 8 caracteres
     if (cep.length !== 8) {
-        alert('O CEP deve ter exatamente 8 caracteres.');
+        mensagem.textContent = 'O CEP deve ter exatamente 8 caracteres.'; //faz aparecer uma mensagem de erro
+        resultado.textContent = ''; //limpa a mensagem anterior
         return;
     }
 
     let url = `https://viacep.com.br/ws/${cep}/json/`;
 
-//retorna as informações como Endereço, complemento, bairro, cidade e estado
+    //requisita para a url
     fetch(url)
-        .then(response => response.json())
+        .then(response => response.json()) //converte a resposta para json
         .then(data => {
             if (data.erro) {
-                document.getElementById('resultado').innerHTML = 'CEP não encontrado.';
+                mensagem.textContent = 'CEP não encontrado.'; //faz exibir uma mensagem de erro caso tenha
+                resultado.textContent = ''; //limpa a mensagem anterior
             } else {
-                document.getElementById('resultado').innerHTML = `
+                mensagem.textContent = ''; // limpa a mensagem anterior
+                resultado.innerHTML = `
                     <p>CEP: ${data.cep}</p>
                     <p>Endereço: ${data.logradouro}</p>
                     <p>Complemento: ${data.complemento}</p>
                     <p>Bairro: ${data.bairro}</p>
                     <p>Cidade: ${data.localidade}</p>
                     <p>Estado: ${data.uf}</p>
-                `;
+                `; //exibe as informações do cep que foi digitado acima
             }
         })
-
-//faz que caso o cep não seja encontrado retorne uma mensagem
         .catch(error => {
-            console.error('Ocorreu um erro na busca:', error);
+            console.error('Ocorreu um erro na busca:', error); //exibe uma informação de erro caso não ache o cep
         });
 }
